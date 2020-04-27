0bd84f014f813956a8834268b79526cc58b931a5
Jaime Biernaski <>
2020-04-27 20:04:02 -0300

    app 02 - I Am Poor

---
+++ app_02_i_am_poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jaime Biernaski on 27/04/20.
+//  Copyright © 2020 Jaime Biernaski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
